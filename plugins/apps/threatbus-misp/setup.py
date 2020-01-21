c981c59be486c6a6d268938ea5cc2806ec1398c3
Felix Ortmann <felix.ortmann@tenzir.com>
2020-01-21 17:00:44 +0100

    Add Kafka support to MISP plugin

--- plugins/apps/threatbus-misp/setup.py
+++ plugins/apps/threatbus-misp/setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(
     name="threatbus-misp",
-    install_requires=["threatbus", "pymisp", "pyzmq"],
+    install_requires=[
+        "threatbus>=0.2.0",
+        "pymisp>=2.4.120",
+        "pyzmq>=18.1.1",
+        "confluent-kafka>=1.3.0",
+    ],
     entry_points={"threatbus.app": ["misp = threatbus_misp"]},
     py_modules=["threatbus_misp", "misp_message_mapping"],
 )
--- plugins/apps/threatbus-misp/threatbus_misp.py
+++ plugins/apps/threatbus-misp/threatbus_misp.py
@@ -1,13 +1,15 @@
+from confluent_kafka import Consumer, KafkaError
 import json
-import warnings
-
-warnings.simplefilter("ignore")  # pymisp produces urllib warnings
 import pymisp
 from queue import Queue
 import threading
+import warnings
 import zmq
+
 from misp_message_mapping import map_to_internal, map_to_misp
 import threatbus
+
+warnings.simplefilter("ignore")  # pymisp produces urllib warnings
 
 """MISP - Open Source Threat Intelligence Platform - plugin for Threat Bus"""
 
@@ -16,15 +18,25 @@ plugin_name = "misp"
 
 def validate_config(config):
     assert config, "config must not be None"
-    # redact fallback value to allow for omitting API configuration
+    # redact fallback values to allow for omitting configuration blocks
     config["api"].add({})
-    if config["api"].get():
+    config["zmq"].add({})
+    config["kafka"].add({})
+    if config["zmq"].get(dict) and config["kafka"].get(dict):
+        raise AssertionError("either use ZeroMQ or Kafka, but not both")
+
+    if config["api"].get(dict):
         config["api"]["host"].get(str)
         config["api"]["ssl"].get(bool)
         config["api"]["key"].get(str)
-    config["zmq"].get(object)
-    config["zmq"]["host"].get(str)
-    config["zmq"]["port"].get(int)
+    if config["zmq"].get(dict):
+        config["zmq"]["host"].get(str)
+        config["zmq"]["port"].get(int)
+    if config["kafka"].get(dict):
+        config["kafka"]["topics"].get(list)
+        config["kafka"]["poll_interval"].add(1.0)
+        config["kafka"]["poll_interval"].get(float)
+        config["kafka"]["config"].get(dict)
 
 
 def publish_sightings(logger, misp, outq):
@@ -44,8 +56,38 @@ def publish_sightings(logger, misp, outq):
         misp.add_sighting(misp_sighting)
 
 
-def receive(logger, zmq_config, inq):
-    """Binds a listener for the the given host/port to the broker ep. Forwards all received messages to the inq.
+def receive_kafka(logger, kafka_config, inq):
+    """Binds a Kafka consumer to the the given host/port. Forwards all received messages to the inq.
+        @param logger A logging.logger object
+        @param kafka_config A configuration object for Kafka binding
+        @param inq The queue to which intel items from MISP are forwarded to
+    """
+    consumer = Consumer(kafka_config["config"].get(dict))
+    consumer.subscribe(kafka_config["topics"].get(list))
+    while True:
+        message = consumer.poll(timeout=kafka_config["poll_interval"].get(float))
+        if message is None:
+            continue
+        if message.error():
+            logger.error(f"Kafka error: {message.error()}")
+            continue
+        try:
+            msg = json.loads(message.value())
+        except Exception as e:
+            logger.error(f"Error decoding Kafka message: {e}")
+            continue
+        if not msg.get("Attribute", None):
+            logger.debug("Skipping message without MISP Attribute")
+            continue
+        intel = map_to_internal(msg["Attribute"], msg.get("action", None), logger)
+        if not intel:
+            logger.debug(f"Discarding unparsable intel {msg['Attribute']}")
+        else:
+            inq.put(intel)
+
+
+def receive_zmq(logger, zmq_config, inq):
+    """Binds a ZMQ poller to the the given host/port. Forwards all received messages to the inq.
         @param logger A logging.logger object
         @param zmq_config A configuration object for ZeroMQ binding
         @param inq The queue to which intel items from MISP are forwarded to
@@ -63,7 +105,11 @@ def receive(logger, zmq_config, inq):
         if socket in socks and socks[socket] == zmq.POLLIN:
             raw = socket.recv()
             _, message = raw.decode("utf-8").split(" ", 1)
-            msg = json.loads(message)
+            try:
+                msg = json.loads(message)
+            except Exception as e:
+                logger.error(f"Erro decoding message {message}: {e}")
+                continue
             if not msg.get("Attribute", None):
                 continue
             intel = map_to_internal(msg["Attribute"], msg.get("action", None), logger)
@@ -98,12 +144,17 @@ def run(config, logging, inq, subscribe_callback, unsubscribe_callback):
 
     # TODO: make individual subscriptions per subscribed MISP endpoint
     subscribe_callback("tenzir/threatbus/sighting", outq)
+
     if misp:
         threading.Thread(
             target=publish_sightings, args=(logger, misp, outq), daemon=True
         ).start()
-
-    threading.Thread(
-        target=receive, args=(logger, config["zmq"], inq), daemon=True
-    ).start()
+    if config["zmq"].get():
+        threading.Thread(
+            target=receive_zmq, args=(logger, config["zmq"], inq), daemon=True
+        ).start()
+    if config["kafka"].get():
+        threading.Thread(
+            target=receive_kafka, args=(logger, config["kafka"], inq), daemon=True
+        ).start()
     logger.info("MISP plugin started")
