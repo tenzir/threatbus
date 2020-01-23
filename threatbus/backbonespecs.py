822a6ce83c8df2206ce8a49339bc33ef3fde8d8a
Felix Ortmann <felix.ortmann@tenzir.com>
2020-01-23 17:15:40 +0100

    Handle snapshot requests in threatbus

--- threatbus/backbonespecs.py
+++ threatbus/backbonespecs.py
@@ -6,23 +6,31 @@ hookspec = pluggy.HookspecMarker("threatbus.backbone")
 @hookspec
 def run(config, logging, inq):
     """Runs / starts a plugin spec with a configuration object
-    :param config a configuration object for the app
-    :param logging a configuration object for the logger
-    :param inq the queue on which all received data arrives
+        @param config A configuration object for the app
+        @param logging A configuration object for the logger
+        @param inq The queue on which all received data arrives
     """
 
 
 @hookspec
-def subscribe(topics, q):
-    """Subscribes the given queue to the requested topics
-    :param topics subscribe to these topics
-    :param q a queue object to forward all messages for the given topics
+def subscribe(topic, q):
+    """Subscribes the given queue to the requested topic.
+        @param topic Subscribe to this topic (string)
+        @param q A queue object to forward all messages for the given topic
     """
 
 
 @hookspec
-def unsubscribe(topics, q):
-    """Unubscribes the given queue from the requested topics
-    :param topics unsubscribe from these topics
-    :param q the queue object that was subscribed to the given topics
+def unsubscribe(topic, q):
+    """Unubscribes the given queue from the requested topic
+        @param topic Unsubscribe from this topic (string)
+        @param q The queue object that was subscribed to the given topic
+    """
+
+
+@hookspec
+def provision_p2p(src_q, dst_q):
+    """Provisions all messages from src_q to dst_q
+        @param src_q The queue to read messages from
+        @param dst_q The queue to write messages to
     """
