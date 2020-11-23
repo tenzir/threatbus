76c085757d4981db4dc5e98565e1429d392441a4
Felix Ortmann <flx.ortmann@gmail.com>
2020-11-23 13:44:36 +0100

    Expose stop function

--- threatbus/stoppable_worker.py
+++ threatbus/stoppable_worker.py
@@ -4,7 +4,7 @@ from threading import Event, Thread
 class StoppableWorker(Thread):
     """
     A threading.Thread with a dedicated method, called _running(), for checking
-    if the thread should continue running. Invoking it's join() method changes
+    if the thread should continue running. Invoking it's stop() method changes
     the return value of _running(). Use this method as exit condition to model
     semi-infinite loops.
     """
@@ -13,12 +13,10 @@ class StoppableWorker(Thread):
         super(StoppableWorker, self).__init__()
         self._stop_event = Event()
 
-    def __stop(self):
+    def stop(self):
+        """Stops the current worker Thread"""
         self._stop_event.set()
 
     def _running(self):
+        """Private predicate to test if this worker should keep on running"""
         return not self._stop_event.is_set()
-
-    def join(self, *args, **kwargs):
-        self.__stop()
-        super(StoppableWorker, self).join(*args, **kwargs)
