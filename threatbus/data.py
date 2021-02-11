67a0020479d44496b8d6a7d8e0a62344ba6522cb
Felix Ortmann <flx.ortmann@gmail.com>
2021-02-11 08:53:48 +0100

    Use stix 2.1 as internal format

--- threatbus/data.py
+++ threatbus/data.py
@@ -1,9 +1,9 @@
-import copy
 from dataclasses import dataclass
-from datetime import datetime, timedelta
-from dateutil import parser
+from datetime import timedelta
 from enum import auto, Enum, unique
 import json
+from stix2 import Indicator, Sighting, parse
+from stix2.parsing import dict_to_stix2
 from typing import Union
 
 
@@ -20,211 +20,53 @@ class Unsubscription:
 
 @unique
 class Operation(Enum):
-    ADD = "ADD"
+    EDIT = "EDIT"
     REMOVE = "REMOVE"
 
 
 @unique
 class MessageType(Enum):
-    INTEL = auto()
+    INDICATOR = auto()
     SIGHTING = auto()
 
 
-@unique
-class IntelType(Enum):
-    IPSRC = auto()
-    IPDST = auto()
-    IPSRC_PORT = auto()
-    IPDST_PORT = auto()
-    EMAILSRC = auto()
-    EMAILDST = auto()
-    TARGETEMAIL = auto()
-    EMAILATTACHMENT = auto()
-    FILENAME = auto()
-    HOSTNAME = auto()
-    DOMAIN = auto()
-    DOMAIN_IP = auto()
-    URL = auto()
-    URI = auto()
-    USERAGENT = auto()
-    MD5 = auto()
-    MALWARESAMPLE = auto()
-    FILENAME_MD5 = auto()
-    SHA1 = auto()
-    FILENAME_SHA1 = auto()
-    SHA256 = auto()
-    FILENAME_SHA256 = auto()
-    X509FINGERPRINTSHA1 = auto()
-    PDB = auto()
-    AUTHENTIHASH = auto()
-    SSDEEP = auto()
-    IMPHASH = auto()
-    PEHASH = auto()
-    IMPFUZZY = auto()
-    SHA224 = auto()
-    SHA384 = auto()
-    SHA512 = auto()
-    SHA512_224 = auto()
-    SHA512_256 = auto()
-    TLSH = auto()
-    CDHASH = auto()
-    FILENAME_AUTHENTIHASH = auto()
-    FILENAME_SSDEEP = auto()
-    FILENAME_IMPHASH = auto()
-    FILENAME_PEHASH = auto()
-    FILENAME_IMPFUZZY = auto()
-    FILENAME_SHA224 = auto()
-    FILENAME_SHA384 = auto()
-    FILENAME_SHA512 = auto()
-    FILENAME_SHA512_224 = auto()
-    FILENAME_SHA512_256 = auto()
-    FILENAME_TLSH = auto()
-
-
-class IntelData(dict):
-    """Threat Bus intel data is a dictionary with at least two keys: 'indicator' and 'intel_type'.
-    The 'indicator' is a tuple of strings, defining the IoC(s)
-    The 'intel_type' is a threatbus.data.IntelType
+@dataclass()
+class Update:
+    """
+    An Update consists of at least an ID and Operation. The operation should be
+    applied to items with the updated ID.
+    TODO: specify content changes
     """
 
-    def __init__(
-        self, indicator: Union[str, tuple], intel_type: IntelType, *args, **kw
-    ):
-        super(IntelData, self).__init__(*args, **kw)
-        assert indicator, "Intel indicator must be set"
-        assert (
-            isinstance(indicator, tuple)
-            and all(map(lambda e: isinstance(e, str), indicator))
-            or isinstance(indicator, str)
-        ), "Intel indicator must either be a string or tuple of strings"
-        assert intel_type, "Intel type must be set"
-        assert isinstance(
-            intel_type, IntelType
-        ), "Intel type must be of type threatbus.data.IntelType"
-        self["indicator"] = indicator if isinstance(indicator, tuple) else (indicator,)
-        self["intel_type"] = intel_type
-
-
-@dataclass()
-class Intel:
-    ts: datetime
     id: str
-    data: IntelData
     operation: Operation
-
-
-@dataclass()
-class Sighting:
-    ts: datetime
-    intel: str
-    context: dict
-    ioc: Union[tuple, None]
 
 
 @dataclass()
 class SnapshotEnvelope:
     """
-    SnapshotEnvelopes are used to wrap intel items or sightings in response to
+    SnapshotEnvelopes wrap indicators or sightings in response to
     SnapshotRequests. The envelope carries additional information, such as the
     MessageType and the unique ID of a snapshot.
     """
 
     snapshot_type: MessageType
     snapshot_id: str
-    body: Union[Intel, Sighting]
+    body: Union[Indicator, Sighting]
 
 
 @dataclass
 class SnapshotRequest:
     """
-    Threat Bus creates SnapshotRequests when apps specifically as for a snapshot
-    during subscription. SnapshotRequests are provisioned via the backbones.
-    A request consists of the requested MessageType (either INTEL or SIGHTING),
-    a unique ID, and the snapshot time delta (e.g., 30 days).
+    Threat Bus creates SnapshotRequests when apps specifically ask for a
+    snapshot during subscription. SnapshotRequests are provisioned through the
+    backbones. A SnapshotRequest consists of the requested MessageType (either
+    INDICATOR or SIGHTING), a unique ID, and the snapshot time delta (e.g., 5 days).
     """
 
     snapshot_type: MessageType
     snapshot_id: str
     snapshot: timedelta
-
-
-class IntelEncoder(json.JSONEncoder):
-    """
-    Encodes Intel objects to JSON strings
-    """
-
-    def default(self, intel: Intel):
-        if type(intel) is not Intel:
-            # let the base class default method raise the TypeError
-            return json.JSONEncoder.default(self, intel)
-        data = copy.deepcopy(intel.data)
-        data["indicator"] = list(intel.data["indicator"])
-        data["intel_type"] = int(intel.data["intel_type"].value)
-        return {
-            "ts": str(intel.ts),
-            "id": str(intel.id),
-            "data": data,
-            "operation": str(intel.operation.value),
-        }
-
-
-class IntelDecoder(json.JSONDecoder):
-    """
-    Decodes JSON strings to Intel objects
-    """
-
-    def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.decode_hook, *args, **kwargs)
-
-    def decode_hook(self, dct: dict):
-        if "intel_type" in dct and "indicator" in dct:
-            # parse IntelData
-            intel_type = IntelType(int(dct.pop("intel_type")))
-            indicator = tuple(dct.pop("indicator"))
-            return IntelData(indicator, intel_type, **dct)
-        elif "ts" in dct and "id" in dct and "data" in dct and "operation" in dct:
-            # parse Intel
-            return Intel(
-                parser.parse(dct["ts"]),
-                dct["id"],
-                dct["data"],
-                Operation(dct["operation"]),
-            )
-        return dct
-
-
-class SightingEncoder(json.JSONEncoder):
-    """
-    Encodes Sighting objects to JSON strings
-    """
-
-    def default(self, sighting: Sighting):
-        if type(sighting) is not Sighting:
-            # let the base class default method raise the TypeError
-            return json.JSONEncoder.default(self, sighting)
-        return {
-            "ts": str(sighting.ts),
-            "intel": sighting.intel,
-            "context": sighting.context,
-            "ioc": list(sighting.ioc) if sighting.ioc else None,
-        }
-
-
-class SightingDecoder(json.JSONDecoder):
-    """
-    Decodes JSON strings to Sighting objects
-    """
-
-    def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(self, object_hook=self.decode_hook, *args, **kwargs)
-
-    def decode_hook(self, dct: dict):
-        if "ts" in dct and "intel" in dct and "context" in dct:
-            ioc = dct.get("ioc", None)
-            if ioc:
-                ioc = tuple(ioc)
-            return Sighting(parser.parse(dct["ts"]), dct["intel"], dct["context"], ioc)
-        return dct
 
 
 class SnapshotRequestEncoder(json.JSONEncoder):
@@ -237,6 +79,7 @@ class SnapshotRequestEncoder(json.JSONEncoder):
             # let the base class default method raise the TypeError
             return json.JSONEncoder.default(self, req)
         return {
+            "type": type(SnapshotRequest).__name__.lower(),
             "snapshot_type": int(req.snapshot_type.value),
             "snapshot_id": str(req.snapshot_id),
             "snapshot": int(req.snapshot.total_seconds()),
@@ -252,13 +95,15 @@ class SnapshotRequestDecoder(json.JSONDecoder):
         json.JSONDecoder.__init__(self, object_hook=self.decode_hook, *args, **kwargs)
 
     def decode_hook(self, dct: dict):
+        type_ = dct.get("type", None)
+        if not type_ or type_ != type(SnapshotRequest).__name__.lower():
+            return dct
         if "snapshot_type" in dct and "snapshot_id" in dct and "snapshot" in dct:
             return SnapshotRequest(
                 MessageType(int(dct["snapshot_type"])),
                 dct["snapshot_id"],
                 timedelta(seconds=dct["snapshot"]),
             )
-        return dct
 
 
 class SnapshotEnvelopeEncoder(json.JSONEncoder):
@@ -270,18 +115,13 @@ class SnapshotEnvelopeEncoder(json.JSONEncoder):
         if type(env) is not SnapshotEnvelope:
             # let the base class default method raise the TypeError
             return json.JSONEncoder.default(self, env)
-        encoder = None
-        if type(env.body) is Intel:
-            encoder = IntelEncoder
-        elif type(env.body) is Sighting:
-            encoder = SightingEncoder
-        else:
-            # let the base class default method raise the TypeError
-            return json.JSONEncoder.default(self, env)
         return {
+            "type": type(SnapshotEnvelope).__name__.lower(),
             "snapshot_type": int(env.snapshot_type.value),
             "snapshot_id": str(env.snapshot_id),
-            "body": encoder.default(self, env.body),
+            "body": json.loads(
+                env.body.serialize()
+            ),  # read back json to avoid double serialization
         }
 
 
@@ -294,22 +134,18 @@ class SnapshotEnvelopeDecoder(json.JSONDecoder):
         json.JSONDecoder.__init__(self, object_hook=self.decode_hook, *args, **kwargs)
 
     def decode_hook(self, dct: dict):
-        if "snapshot_type" in dct and "snapshot_id" in dct and "body" in dct:
-            snapshot_type = MessageType(int(dct["snapshot_type"]))
-            return SnapshotEnvelope(
-                snapshot_type,
-                dct["snapshot_id"],
-                dct["body"],
-            )
-        if (
-            "intel_type" in dct
-            and "indicator" in dct
-            or "ts" in dct
-            and "id" in dct
-            and "data" in dct
-            and "operation" in dct
-        ):
-            return IntelDecoder.decode_hook(self, dct)
-        elif "ts" in dct and "intel" in dct and "context" in dct:
-            return SightingDecoder.decode_hook(self, dct)
-        return dct
+        type_ = dct.get("type", None)
+        if not type_:
+            return dct
+        if type_ != type(SnapshotEnvelope).__name__.lower():
+            # decoders walk through nested objects first (bottom up).
+            # try to parse nested stix2 objs per best-effort, else bubble up
+            try:
+                return dict_to_stix2(dct, allow_custom=True)
+            except Exception:
+                return dct
+        return SnapshotEnvelope(
+            MessageType(int(dct["snapshot_type"])),
+            dct["snapshot_id"],
+            parse(dct["body"], allow_custom=True),
+        )
--- threatbus/test_data.py
+++ threatbus/test_data.py
@@ -2,19 +2,14 @@ from datetime import datetime, timedelta
 import unittest
 import json
 
+from stix2 import Indicator, Sighting
+from stix2.utils import format_datetime
+
 from data import (
-    Intel,
-    IntelData,
-    IntelType,
     MessageType,
     Operation,
-    Sighting,
     SnapshotEnvelope,
     SnapshotRequest,
-    IntelEncoder,
-    IntelDecoder,
-    SightingEncoder,
-    SightingDecoder,
     SnapshotRequestEncoder,
     SnapshotRequestDecoder,
     SnapshotEnvelopeEncoder,
@@ -24,16 +19,21 @@ from data import (
 
 class TestJsonConversions(unittest.TestCase):
     def setUp(self):
-        self.ts = datetime.now().astimezone()
-        self.intel_id = "intel-42"
-        self.indicator = "6.6.6.6"
-        self.intel_type = IntelType.IPSRC
-        self.operation = Operation.ADD
-        self.intel_data = IntelData(
-            self.indicator, self.intel_type, foo=23, more_args="MORE ARGS"
+        self.created = datetime.now().astimezone()
+        self.indicator_id = "indicator--df0f9a0e-c3b6-4f53-b0cf-5e9c454ee0cc"
+        self.pattern = "[ipv4-addr:value = '6.6.6.6']"
+        self.pattern_type = "stix2"
+        self.operation = Operation.REMOVE
+        self.indicator = Indicator(
+            id=self.indicator_id,
+            pattern=self.pattern,
+            pattern_type=self.pattern_type,
+            created=self.created,
+            valid_from=self.created,
+            modified=self.created,
         )
-        self.intel = Intel(self.ts, self.intel_id, self.intel_data, self.operation)
 
+        self.sighting_id = "sighting--df0f9a0e-c3b6-4f53-b0cf-5e9c454ee0cc"
         self.sighting_context = {
             "ts": "2017-03-03T23:56:09.652643840",
             "uid": "CMeLkt11aTqwgN4FI9",
@@ -66,7 +66,14 @@ class TestJsonConversions(unittest.TestCase):
             "source": "VAST",
         }
         self.sighting = Sighting(
-            self.ts, self.intel_id, self.sighting_context, (self.indicator,)
+            id=self.sighting_id,
+            created=self.created,
+            modified=self.created,
+            sighting_of_ref=self.indicator_id,
+            custom_properties={
+                "x_threatbus_sighting_context": self.sighting_context,
+                "x_threatbus_indicator": self.indicator,
+            },
         )
 
         self.snapshot_id = "SNAPSHOT_UUID"
@@ -75,61 +82,12 @@ class TestJsonConversions(unittest.TestCase):
             MessageType.SIGHTING, self.snapshot_id, self.snapshot
         )
 
-        self.snapshot_envelope_intel = SnapshotEnvelope(
-            MessageType.INTEL, self.snapshot_id, self.intel
+        self.snapshot_envelope_indicator = SnapshotEnvelope(
+            MessageType.INDICATOR, self.snapshot_id, self.indicator
         )
         self.snapshot_envelope_sighting = SnapshotEnvelope(
             MessageType.SIGHTING, self.snapshot_id, self.sighting
         )
-
-    def test_valid_intel_encoding(self):
-        encoded = json.dumps(self.intel, cls=IntelEncoder)
-        # encoding cannot be compared as string, because order of the fileds in
-        # the string representation is not fixed.
-        # We cast the JSON back to a simple python dict and them compare values
-        py_dict = json.loads(encoded)
-        self.assertEqual(py_dict["ts"], str(self.ts))
-        self.assertEqual(py_dict["id"], self.intel_id)
-        self.assertEqual(
-            py_dict["data"],
-            {
-                "foo": 23,
-                "more_args": "MORE ARGS",
-                "indicator": [self.indicator],
-                "intel_type": self.intel_type.value,
-            },
-        )
-
-    def test_valid_intel_decoding(self):
-        encoded = f'{{"ts": "{self.ts}", "id": "intel-42", "data": {{"foo": 23, "more_args": "MORE ARGS", "indicator": ["{self.indicator}"], "intel_type": "{self.intel_type.value}"}}, "operation": "{self.operation.value}"}}'
-        intel = json.loads(encoded, cls=IntelDecoder)
-        self.assertEqual(intel, self.intel)
-
-    def test_intel_encoding_roundtrip(self):
-        encoded = json.dumps(self.intel, cls=IntelEncoder)
-        read_back = json.loads(encoded, cls=IntelDecoder)
-        self.assertEqual(read_back, self.intel)
-
-    def test_valid_sighting_encoding(self):
-        encoded = json.dumps(self.sighting, cls=SightingEncoder)
-        # encoding cannot be compared as string, because order of the fileds in
-        # the string representation is not fixed.
-        # We cast the JSON back to a simple python dict and them compare values
-        py_dict = json.loads(encoded)
-        self.assertEqual(py_dict["ts"], str(self.ts))
-        self.assertEqual(py_dict["intel"], self.intel_id)
-        self.assertEqual(py_dict["ioc"], [self.indicator])
-        self.assertEqual(py_dict["context"], self.sighting_context)
-
-    def test_valid_sighting_decoding(self):
-        encoded = f'{{"ts": "{self.ts}", "intel": "{self.intel_id}", "ioc": ["{self.indicator}"], "context": {json.dumps(self.sighting_context)}}}'
-        sighting = json.loads(encoded, cls=SightingDecoder)
-        self.assertEqual(sighting, self.sighting)
-
-    def test_sighting_encoding_roundtrip(self):
-        encoded = json.dumps(self.sighting, cls=SightingEncoder)
-        read_back = json.loads(encoded, cls=SightingDecoder)
-        self.assertEqual(read_back, self.sighting)
 
     def test_valid_snapshot_request_encoding(self):
         encoded = json.dumps(self.snapshot_request, cls=SnapshotRequestEncoder)
@@ -140,9 +98,15 @@ class TestJsonConversions(unittest.TestCase):
         self.assertEqual(py_dict["snapshot_type"], MessageType.SIGHTING.value)
         self.assertEqual(py_dict["snapshot_id"], self.snapshot_id)
         self.assertEqual(py_dict["snapshot"], self.snapshot.total_seconds())
+        self.assertEqual(py_dict["type"], type(SnapshotRequest).__name__.lower())
 
     def test_valid_snapshot_request_decoding(self):
-        encoded = f'{{"snapshot_type": {MessageType.SIGHTING.value}, "snapshot_id": "{self.snapshot_id}", "snapshot": {self.snapshot.total_seconds()}}}'
+        encoded = f"""{{
+            "type": "{type(SnapshotRequest).__name__.lower()}",
+            "snapshot_type": {MessageType.SIGHTING.value},
+            "snapshot_id": "{self.snapshot_id}",
+            "snapshot": {self.snapshot.total_seconds()}
+        }}"""
         read_back = json.loads(encoded, cls=SnapshotRequestDecoder)
         self.assertEqual(read_back, self.snapshot_request)
 
@@ -152,8 +116,8 @@ class TestJsonConversions(unittest.TestCase):
         self.assertEqual(read_back, self.snapshot_request)
 
     def test_valid_snapshot_envelope_encoding(self):
-        encoded_envelope_intel = json.dumps(
-            self.snapshot_envelope_intel, cls=SnapshotEnvelopeEncoder
+        encoded_envelope_indicator = json.dumps(
+            self.snapshot_envelope_indicator, cls=SnapshotEnvelopeEncoder
         )
         encoded_envelope_sighting = json.dumps(
             self.snapshot_envelope_sighting, cls=SnapshotEnvelopeEncoder
@@ -161,36 +125,68 @@ class TestJsonConversions(unittest.TestCase):
         # encoding cannot be compared as string, because order of the fileds in
         # the string representation is not fixed.
         # We cast the JSON back to a simple python dict and them compare values
-        py_dict = json.loads(encoded_envelope_intel)
-        self.assertEqual(py_dict["snapshot_type"], MessageType.INTEL.value)
+        py_dict = json.loads(encoded_envelope_indicator)
+        self.assertEqual(py_dict["snapshot_type"], MessageType.INDICATOR.value)
         self.assertEqual(py_dict["snapshot_id"], self.snapshot_id)
-        self.assertEqual(py_dict["body"]["ts"], str(self.ts))
-        self.assertEqual(py_dict["body"]["id"], self.intel_id)
-        self.assertEqual(py_dict["body"]["operation"], self.operation.value)
-        self.assertEqual(py_dict["body"]["data"]["indicator"][0], self.indicator)
-        self.assertEqual(py_dict["body"]["data"]["intel_type"], self.intel_type.value)
+        self.assertEqual(py_dict["body"]["created"], format_datetime(self.created))
+        self.assertEqual(py_dict["body"]["id"], self.indicator_id)
+        self.assertEqual(py_dict["body"]["pattern"], self.pattern)
+        self.assertEqual(py_dict["body"]["pattern_type"], self.pattern_type)
+        self.assertEqual(py_dict["type"], type(SnapshotEnvelope).__name__.lower())
 
         py_dict = json.loads(encoded_envelope_sighting)
         self.assertEqual(py_dict["snapshot_type"], MessageType.SIGHTING.value)
         self.assertEqual(py_dict["snapshot_id"], self.snapshot_id)
-        self.assertEqual(py_dict["body"]["ts"], str(self.ts))
-        self.assertEqual(py_dict["body"]["intel"], self.intel_id)
-        self.assertEqual(py_dict["body"]["ioc"], [self.indicator])
-        self.assertEqual(py_dict["body"]["context"], self.sighting_context)
+        self.assertEqual(py_dict["body"]["created"], format_datetime(self.created))
+        self.assertEqual(py_dict["body"]["sighting_of_ref"], self.indicator_id)
+        self.assertEqual(
+            py_dict["body"]["x_threatbus_sighting_context"], self.sighting_context
+        )
+        self.assertEqual(py_dict["type"], type(SnapshotEnvelope).__name__.lower())
 
     def test_valid_snapshot_envelope_decoding(self):
-        encoded_envelope_intel = f'{{"snapshot_type": {MessageType.INTEL.value}, "snapshot_id": "{self.snapshot_id}", "body": {{"ts": "{self.ts}", "id": "{self.intel_id}", "data": {{"foo": 23, "more_args": "MORE ARGS", "indicator": ["{self.indicator}"], "intel_type": "{self.intel_type.value}"}}, "operation": "{self.operation.value}"}}}}'
-        read_back = json.loads(encoded_envelope_intel, cls=SnapshotEnvelopeDecoder)
-        self.assertEqual(read_back, self.snapshot_envelope_intel)
+        encoded_envelope_indicator = f"""{{
+            "type": "{type(SnapshotEnvelope).__name__.lower()}",
+            "snapshot_type": {MessageType.INDICATOR.value},
+            "snapshot_id": "{self.snapshot_id}",
+            "body": {{
+                "id": "{self.indicator_id}",
+                "pattern": "{self.pattern}",
+                "pattern_type": "stix2",
+                "created": "{format_datetime(self.created)}",
+                "type": "indicator",
+                "spec_version": "2.1",
+                "modified": "{format_datetime(self.created)}",
+                "valid_from": "{format_datetime(self.created)}"
+            }}
+        }}"""
+        read_back = json.loads(encoded_envelope_indicator, cls=SnapshotEnvelopeDecoder)
+        self.assertEqual(read_back, self.snapshot_envelope_indicator)
 
-        encoded_envelope_sighting = f'{{"snapshot_type": {MessageType.SIGHTING.value}, "snapshot_id": "{self.snapshot_id}", "body": {{"ts": "{self.ts}", "intel": "{self.intel_id}", "ioc": ["{self.indicator}"], "context": {json.dumps(self.sighting_context)}}}}}'
+        encoded_envelope_sighting = f"""{{
+            "type": "{type(SnapshotEnvelope).__name__.lower()}",
+            "snapshot_type": {MessageType.SIGHTING.value},
+            "snapshot_id": "{self.snapshot_id}",
+            "body": {{
+                "created": "{format_datetime(self.created)}",
+                "modified": "{format_datetime(self.created)}",
+                "sighting_of_ref": "{self.indicator_id}",
+                "type": "sighting",
+                "spec_version": "2.1",
+                "id": "{self.sighting_id}",
+                "x_threatbus_sighting_context": {json.dumps(self.sighting_context)},
+                "x_threatbus_indicator": {self.indicator.serialize()}
+            }}
+        }}"""
         read_back = json.loads(encoded_envelope_sighting, cls=SnapshotEnvelopeDecoder)
         self.assertEqual(read_back, self.snapshot_envelope_sighting)
 
     def test_snapshot_envelope_encoding_roundtrip(self):
-        encoded = json.dumps(self.snapshot_envelope_intel, cls=SnapshotEnvelopeEncoder)
+        encoded = json.dumps(
+            self.snapshot_envelope_indicator, cls=SnapshotEnvelopeEncoder
+        )
         read_back = json.loads(encoded, cls=SnapshotEnvelopeDecoder)
-        self.assertEqual(read_back, self.snapshot_envelope_intel)
+        self.assertEqual(read_back, self.snapshot_envelope_indicator)
 
         encoded = json.dumps(
             self.snapshot_envelope_sighting, cls=SnapshotEnvelopeEncoder
