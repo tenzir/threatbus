b93242adcc20407aeecefac24c7249d2c1b63dee
Felix Ortmann <flx.ortmann@gmail.com>
2021-03-02 15:11:42 +0100

    Send more objects by default with the zmq util

--- tests/utils/zmq_sender.py
+++ tests/utils/zmq_sender.py
@@ -2,7 +2,7 @@
 
 import zmq
 import time
-from stix2 import Indicator
+from stix2 import Indicator, Sighting
 
 
 def send(topic, msg, host="127.0.0.1", port=50000, bind=True):
@@ -23,5 +23,9 @@ if __name__ == "__main__":
     indicator = Indicator(
         pattern="[domain-name:value = 'evil.com']", pattern_type="stix"
     )
+    sighting = Sighting(
+        sighting_of_ref="indicator--629a6400-8817-4bcb-aee7-8c74fc57482c",
+        custom_properties={"x_threatbus_source": "VAST"},
+    )
     send("stix2/indicator", indicator.serialize(), port=13372, bind=False)
-    # send("sighting", "hello", bind=False)
+    send("stix2/sighting", sighting.serialize(), port=13372, bind=False)
